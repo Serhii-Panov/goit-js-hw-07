cb83500da969361cdd026ba60f254f89f38a60f0
Serhii Panov <suboxoneforhelp@gmail.com>
2025-08-23 13:16:27 +0300

    all 5 tasks done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,13 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", HandleSubmit);
+function HandleSubmit(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+  const inputs = { email: email.trim(), password: password.trim() };
+  console.log(inputs);
+  form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector(".change-color");
+const background = document.querySelector("body");
+const color = document.querySelector(".color");
+changeColorBtn.addEventListener("click", ChangeColor);
+function ChangeColor(event) {
+  background.style.backgroundColor = getRandomHexColor();
+  color.textContent = background.style.backgroundColor;
+}
